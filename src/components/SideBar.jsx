6d089c13ffc414e651db9bffbb08722b2ad2b7bd
Dranas786 <dranas2002@gmail.com>
2025-01-27 22:59:17 -0700

    start

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>Info about the pic</p>
+        </div>
+      </div>
+    </div>
+  );
+}
